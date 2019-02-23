ed6f54a8d0a54ec9bf7f2897eb453ff38d4b1f65
Niklas Merz <niklasmerz@linux.com>
2019-02-23 17:30:47 +0100

    Read one xml file

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,4 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  fileToUpload: File = null;
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+
+    console.log(this.fileToUpload);
+
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const readXml = e.target.result;
+      console.log(readXml);
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(readXml, 'application/xml');
+      console.log(doc);
+    };
+    reader.readAsText(this.fileToUpload);
+  }
+}
