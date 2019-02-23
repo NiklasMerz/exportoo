0b5eadc63fe35778d96c957cfcadd87323eb72a8
Niklas Merz <niklasmerz@linux.com>
2019-02-23 12:29:12 +0100

    Sqlite build error module typing

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { Database } from 'sqlite3';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  constructor() {
+    const db = new Database(':memory:');
+    db.serialize(function () {
+      db.run('CREATE TABLE lorem (info TEXT)');
+
+      const stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+      for (let i = 0; i < 10; i++) {
+        stmt.run('Ipsum ' + i);
+      }
+      stmt.finalize();
+
+      db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+        console.log(row.id + ': ' + row.info);
+      });
+    });
+
+    db.close();
+  }
+}
